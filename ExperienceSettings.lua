d8f09fde83c0044f708b22948ef916251394c684
White-rbx <kogpasan14@gmail.com>
2025-10-08 18:24:30 +0700

    Update ExperienceSettings.lua

--- ExperienceSettings.lua
+++ ExperienceSettings.lua
@@ -766,11 +766,15 @@ hr.Size = UDim2.new(0, 350, 1, 0)
 
 -- ===== HRP Watcher System (full) =====
 local Players = game:GetService("Players")
-local gui_aV2 = game:GetService("CoreGui").TopBarApp.TopBarApp.UnibarLeftFrame.HealthBar.ValueFolder.ValueGui
+local CoreGui = game:GetService("CoreGui")
+local hb = CoreGui:WaitForChild("TopBarApp"):WaitForChild("TopBarApp")
+	:WaitForChild("UnibarLeftFrame"):WaitForChild("HealthBar")
+
+local gui_aV2 = hb:FindFirstChild("ValueGui") or hb:FindFirstChild("ValueLabels")
 
 -- interval config
 local CHECK_INTERVAL = 0.05 -- ระยะเวลาเช็ค (วินาที)
-local TIMEOUT = 8.878      -- ถ้าไม่มี HRP เกินค่านี้ให้ปิด UI (วินาที)
+local TIMEOUT = 3.878      -- ถ้าไม่มี HRP เกินค่านี้ให้ปิด UI (วินาที)
 
 -- เก็บตำแหน่งเริ่มต้น
 local wa1_initial_pos, hr_initial_size, mtb_initial_pos
