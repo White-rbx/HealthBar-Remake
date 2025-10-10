9c730ec20fb0e379d41b80c5b5d6a473c4a56da5
White-rbx <kogpasan14@gmail.com>
2025-10-11 01:15:45 +0700

    Update SeeAll.lua

--- SeeAll.lua
+++ SeeAll.lua
@@ -1,10 +1,13 @@
 local CG = game:GetService("CoreGui")
 local VIM = game:GetService("VirtualInputManager")
 
--- Chained WaitForChild for SeeAll
-local SeeAll = CG:WaitForChild("TopBarApp")
-                :WaitForChild("TopBarApp") -- there you go, your stupid TopBarApp
-                :WaitForChild("UnibarLeftFrame")
+-- Outer TopBarApp
+local OuterTopBar = CG:WaitForChild("TopBarApp")
+-- Inner TopBarApp
+local InnerTopBar = OuterTopBar:WaitForChild("TopBarApp")
+
+-- SeeAll button
+local SeeAll = InnerTopBar:WaitForChild("UnibarLeftFrame")
                 :WaitForChild("HealthBar")
                 :WaitForChild("ExperienceSettings")
                 :WaitForChild("Menu")
@@ -13,25 +16,21 @@ local SeeAll = CG:WaitForChild("TopBarApp")
                 :WaitForChild("Pmax")
                 :WaitForChild("SeeAll")
 
--- Chained WaitForChild for Trigger Background
-local TriggerBackground = CG:WaitForChild("TopBarApp")
-                        :WaitForChild("TopBarApp") -- you too, STUPID-
-                        :WaitForChild("MenuIconHolder")
+-- Trigger background for simulating click
+local TriggerBackground = InnerTopBar:WaitForChild("MenuIconHolder")
                         :WaitForChild("TriggerPoint")
                         :WaitForChild("Background")
 
 SeeAll.MouseButton1Click:Connect(function()
     if TriggerBackground and SeeAll then
-        local bg = CG.TopBarApp.UnibarLeftFrame.HealthBar.ExperienceSettings.Menu.Background
-        -- เลื่อน background ไป X = 1
+        -- Move background to X = 1
+        local bg = InnerTopBar.UnibarLeftFrame.HealthBar.ExperienceSettings.Menu.Background
         bg.Position = UDim2.new(1, 0, bg.Position.Y.Scale, bg.Position.Y.Offset)
         
-        -- จำลองคลิกเมนู Roblox
+        -- Simulate Roblox menu click
         local p, s = TriggerBackground.AbsolutePosition, TriggerBackground.AbsoluteSize
         local x, y = p.X + s.X/2, p.Y + s.Y/2
         VIM:SendMouseButtonEvent(x, y, 0, true, game, 0)
         VIM:SendMouseButtonEvent(x, y, 0, false, game, 0)
     end
 end)
-
--- GitHub always let my script latest >:(
