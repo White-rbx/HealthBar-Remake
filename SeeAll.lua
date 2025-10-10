13493b48c1f385ab798a27531e0cd05b78899b52
White-rbx <kogpasan14@gmail.com>
2025-10-11 01:24:12 +0700

    Fixed6

--- SeeAll.lua
+++ SeeAll.lua
@@ -16,30 +16,11 @@ local SeeAll = InnerTopBar:WaitForChild("UnibarLeftFrame")
                 :WaitForChild("Pmax")
                 :WaitForChild("SeeAll")
 
--- Trigger background for simulating click
-local TriggerBackground = InnerTopBar:WaitForChild("MenuIconHolder")
-                        :WaitForChild("TriggerPoint")
-                        :WaitForChild("Background")
-
 -- Menu background for tweening
 local MenuBackground = InnerTopBar.UnibarLeftFrame.HealthBar.ExperienceSettings.Menu.Background
 
 -- Tween info for closing the menu
 local tweenInfo = TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
-
-SeeAll.MouseButton1Click:Connect(function()
-    if TriggerBackground and SeeAll then
-        -- Move Menu Background to X = 1
-        MenuBackground.Position = UDim2.new(1, 0, MenuBackground.Position.Y.Scale, MenuBackground.Position.Y.Offset)
-        
-        -- Simulate click properly by sending both mouse down/up with a small delay
-        local p, s = TriggerBackground.AbsolutePosition, TriggerBackground.AbsoluteSize
-        local x, y = p.X + s.X/2, p.Y + s.Y/2
-        VIM:SendMouseButtonEvent(x, y, 0, true, game, 0)
-        task.wait(0.05)
-        VIM:SendMouseButtonEvent(x, y, 0, false, game, 0)
-    end
-end)
 
 -- Function to close Menu Background with Tween
 local function closeMenuBackground()
@@ -48,13 +29,17 @@ local function closeMenuBackground()
     tween:Play()
 end
 
--- Example: close menu when pressing Escape
-game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)
-    if gameProcessed then return end
-    if input.KeyCode == Enum.KeyCode.Escape then
-        closeMenuBackground()
-    end
+-- Click SeeAll
+SeeAll.MouseButton1Click:Connect(function()
+    -- Move Menu Background to X = 1
+    MenuBackground.Position = UDim2.new(1, 0, MenuBackground.Position.Y.Scale, MenuBackground.Position.Y.Offset)
+    
+    -- Simulate click at top-left of screen (button is there)
+    local x, y = 0.01 * workspace.CurrentCamera.ViewportSize.X, 0.01 * workspace.CurrentCamera.ViewportSize.Y
+    VIM:SendMouseButtonEvent(x, y, 0, true, game, 0)
+    task.wait(0.05)
+    VIM:SendMouseButtonEvent(x, y, 0, false, game, 0)
+    
+    -- Tween Menu background back every time
+    closeMenuBackground()
 end)
-
--- GitHub always doesn't let my script to latest version.
--- GITHUB PLEASE MAKE MY SCRIPT TO LATSET VERSION 
