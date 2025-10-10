000a2771202768b440ef9f4b2bfd028885feeb66
White-rbx <kogpasan14@gmail.com>
2025-10-11 01:51:38 +0700

    Alright - Fixed10

--- SeeAll.lua
+++ SeeAll.lua
@@ -25,7 +25,7 @@ local tweenInfo = TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection
 -- Function to tween Menu background slightly left for visible effect
 local function tweenMenuBackground()
     -- Start from X=1 (already set) and tween slightly left to 0.95
-    local goal = {Position = UDim2.new(0.95, MenuBackground.Position.X.Offset, MenuBackground.Position.Y.Scale, MenuBackground.Position.Y.Offset)}
+    local goal = {Position = UDim2.new(1, MenuBackground.Position.X.Offset, MenuBackground.Position.Y.Scale, MenuBackground.Position.Y.Offset)}
     local tween = TweenService:Create(MenuBackground, tweenInfo, goal)
     tween:Play()
 end
@@ -35,7 +35,7 @@ SeeAll.MouseButton1Click:Connect(function()
     -- Instantly set Menu Background to X=1
     MenuBackground.Position = UDim2.new(1, 0, MenuBackground.Position.Y.Scale, MenuBackground.Position.Y.Offset)
     
-    -- Simulate click at fixed Offset (30,30)
+    -- Simulate click at fixed Offset (80,30)
     local x, y = 80, 30
     VIM:SendMouseButtonEvent(x, y, 0, true, game, 0)
     task.wait(0.05)
