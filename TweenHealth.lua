0deaea8709779a867d93dffcdeecebb81cb7846e
White-rbx <kogpasan14@gmail.com>
2025-10-12 03:50:20 +0700

    L

--- TweenHealth.lua
+++ TweenHealth.lua
@@ -97,7 +97,7 @@ local function setupOverlay()
 		overlayGui = Instance.new("ScreenGui")
 		overlayGui.Name = "DamageOverlay"
 		overlayGui.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
-     overlayGui.Enabled = false
+        overlayGui.Enabled = false
 		overlayGui.Parent = CoreGui
 		overlayImage = nil
 	end
