4597eae7d77d1bc157d36d22689ed53e150179e9
White-rbx <kogpasan14@gmail.com>
2025-10-12 21:14:19 +0700

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -1,4 +1,4 @@
--- ===== [ Service's ] =====
+-- ===== [ Service's ] ===== 
 local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
