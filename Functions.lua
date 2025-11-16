55b0647da9ab89df21baa5dd6a552a7d4a24a7c9
White-rbx <kogpasan14@gmail.com>
2025-11-16 13:37:54 +0700

    Available

--- Functions.lua
+++ Functions.lua
@@ -4,7 +4,7 @@ local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
 
--- ===== [ Positions ] =====
+-- ===== [ Positions ] ===== 
 local Background = game:GetService("CoreGui")
                    :WaitForChild("TopBarApp")
                    :WaitForChild("TopBarApp")
