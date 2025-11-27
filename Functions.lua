c751f26c4e645ff6af46a24542b895839df361d1
White-rbx <kogpasan14@gmail.com>
2025-11-28 06:07:50 +0700

    ....... reloaded bruh

--- Functions.lua
+++ Functions.lua
@@ -1,4 +1,4 @@
--- So uhm just a script lol. 3.3539945
+-- So uhm just a script lol. 3.3539946
 -- ===== [ Service's ] ===== 
 local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
