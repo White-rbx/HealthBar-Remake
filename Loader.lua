20d010bf5b425ae978718e5e75edc7adc236d12a
White-rbx <kogpasan14@gmail.com>
2025-12-06 21:15:07 +0700

    Unavailable

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
--- Well 1.7976
+-- Well 1.7977
 -- Monitor & auto-run (executor)
 local URL = "https://raw.githubusercontent.com/White-rbx/HealthBar-Remake/refs/heads/main/loadstring.lua"
 local CoreGui = game:GetService("CoreGui")
