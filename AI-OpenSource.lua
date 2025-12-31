8573492dd0c37cd5dfdeade290906e8cf3d4082a
White-rbx <kogpasan14@gmail.com>
2026-01-01 02:29:14 +0700

    Removed warning

--- AI-OpenSource.lua
+++ AI-OpenSource.lua
@@ -1,4 +1,4 @@
--- gpt 3.654
+-- gpt 3.655
 
 -- =====>> Saved Functions <<=====
 
@@ -355,7 +355,7 @@ end
 ]]
 
 txt(user.Nill, "Nothing is working! Please wait for the next update!", 180,180,180)
-txt(user.Nill, "Version: Test 3.654 | © Copyright LighterCyan", 180, 180, 180)
+txt(user.Nill, "Version: Test 3.655 | © Copyright LighterCyan", 180, 180, 180)
 txt(user.Warn, "Stop! For your safety, please do not share your API and avoid being stared at by people around you. Due to safety and privacy concerns, you confirm that you will use your API to continue using our AI-OpenSource or not? With respect.", 255,255,0)
 txt(user.Info, "Use /help for more information or commands (SOON)", 0,170,255)
 txt(user.Nill, "[====== Chat ======]", 180, 180, 180)
@@ -688,12 +688,6 @@ local function handleCommand(raw)
     -- /unsaveapi
     if cmd == "/unsaveapi" or cmd == "/unapi" then
         txt(user.Sys, "API unsaved", 255, 90, 0)
-        return true
-    end
-
-    -- dangerous commands
-    if cmd == "/loadstring" or cmd == "/script" then
-        txt(user.Warn, "Command disabled for safety", 255,255,0)
         return true
     end
 
