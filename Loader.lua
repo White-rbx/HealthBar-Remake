674966c868a8f3f8c264b1240b887f70079231a5
White-rbx <kogpasan14@gmail.com>
2026-01-03 05:38:50 +0700

    Update version comment from 1.83 to 1.84

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
--- Well 1.83
+-- Well 1.84
 -- Monitor & auto-run (executor)
 local URL = "https://raw.githubusercontent.com/White-rbx/HealthBar-Remake/refs/heads/main/loadstring.lua"
 local CoreGui = game:GetService("CoreGui")
