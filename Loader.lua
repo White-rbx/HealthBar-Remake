2186f4d176429e5c0d16916da229cdd222941827
White-rbx <kogpasan14@gmail.com>
2026-01-10 13:48:06 +0700

    Upgrade

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
--- Well 1.84
+-- Well 1.85
 -- Monitor & auto-run (executor)
 local URL = "https://raw.githubusercontent.com/White-rbx/HealthBar-Remake/refs/heads/main/loadstring.lua"
 local CoreGui = game:GetService("CoreGui")
@@ -196,6 +196,9 @@ sata:Destroy()
 
 -- AI-OpenSource
 loadstring(game:HttpGet("https://raw.githubusercontent.com/White-rbx/HealthBar-Remake/refs/heads/loadstring/AI-OpenSource.lua"))()
+
+-- Seacrh
+loadstring(game:HttpGet("https://raw.githubusercontent.com/White-rbx/HealthBar-Remake/refs/heads/ExperienceSettings-(loadstring)/Search.lua"))()
 
 task.spawn(function()
 	local StarterGui = game:GetService("StarterGui")
