aa9cd55a5476d539660afe1e597593c1d910ad79
White <kogpasan14@gmail.com>
2026-05-28 20:52:05 +0700

    Removed something

--- AI-OpenSource.lua
+++ AI-OpenSource.lua
@@ -1,4 +1,4 @@
-local ver = " UIs 5.358 "
+local ver = " UIs 5.359 "
 local update = [[
 # -- Update logs --
 (:8/1/2026 | 5:55 pm: !) Fixed bug
@@ -1894,11 +1894,10 @@ local lowFpsTime = 0
 
 local FPS_RULES = {
 
-	[9] = 35,
 	[6] = 25,
 	[4] = 13,
 	[2] = 8,
-	[0] = 0
+	[0] = 5,
 
 }
 
