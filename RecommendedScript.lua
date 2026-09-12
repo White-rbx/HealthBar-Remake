50ab54861ce4f8b3ffb0679f1bcd5d81bc1994d3
White <kogpasan14@gmail.com>
2026-09-12 15:45:50 +0700

    Update RecommendedScript to version 2.3
    Updated script version and modified entries for RoChip and ConsoleCopyButton.

--- RecommendedScript.lua
+++ RecommendedScript.lua
@@ -1,4 +1,4 @@
-local v_ver = [[Recommanded Script 2.2 Remakes]]
+local v_ver = [[Recommanded Script 2.3 Yes]]
 --[[ UI_functions version: 2.3 ( Reduced Locals for more less risk to due Out Of Local ) ]]
 
 ------------------------------------------------------------------------------------------
@@ -408,8 +408,8 @@ end
 add("Infinite yield (@edge.egg | discord)", "The most powerful admin commands.", "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", 150, 50, 50)
 add("Delta Keyboard (@sakura)", "A keyboard for mobile. Crack by Ata", "https://raw.githubusercontent.com/Xxtan31/Ata/main/deltakeyboardcrack.txt")
 add("Dex++ (@mcdaggitt)", "Not my script but Ill actively update it with new features. Includes optimization + rspy plugin!", "https://raw.githubusercontent.com/jodta/my-scripts/refs/heads/main/Dex%2B%2B/Decompiler%20Fix.lua",0,150,0)
-add("RoChip (@Allskill)", "Universal script allows you to tp to players, also lets you fling them and many more.", "https://rawscripts.net/raw/Universal-Script-rochips-universal-18294",155,155,0)
-add("ConsoleCopyButton (@Lordi_scripts)", "lets you copy everything right in the native console interface (/console OR F9)", "https://rawscripts.net/raw/Universal-Script-ConsoleCopyButton-60768",150,0,150)
+add("RoChip (@83937293938381929383)", "Universal script allows you to tp to players, also lets you fling them and many more.", "https://rawscripts.net/raw/Universal-Script-rochips-universal-18294",155,155,0)
+add("ConsoleCopyButton (@Lordi_scripts)", "lets you copy everything right in the native console interface (/console OR F9)", "https://rawscripts.net/raw/Universal-Script-Richie-panel-upd-v1-0-4-18440",150,0,150)
 add("YouTube Music Player V8 (@Turmux404)", "Take your Roblox experience to the next level by listening to your favorite  music or watching YouTube videos while you play.", "https://raw.githubusercontent.com/Dan41/Roblox-Scripts/refs/heads/main/Youtube%20Music%20Player/YoutubeMusicPlayer.lua",220,0,0)
 add("YARHM - MM2, FTF, Forsaken, CNA Sim, etc. (@Imperial)" ,"yet another random hub menu.", "https://rawscripts.net/raw/Universal-Script-YARHM-12403",200,0,200)
 add("AFEM MAX - 40K EMOTES SCRIPT BEST (@Imperial)", "Introducing AFEM Max, the powerful AFEM you know and love and now EVEN MORE supercharged!", "https://rawscripts.net/raw/Universal-Script-AFEM-Max-Open-Alpha-50210",0,150,200)
@@ -424,3 +424,5 @@ add("universal movement predictor (@koboldpaws)", "cyan dot = predicted pos; yel
 add("physics based tail animator (@koboldpaws)", 'works with any ugc with the name "tail" in it', "https://rawscripts.net/raw/Universal-Script-physics-based-tail-animator-86591",255,0,255)
 add("Anti GamePlay Paused (@sillzchibi)", "Removes gameplay puased", "https://rawscripts.net/raw/Universal-Script-Anti-GamePlay-Paused-43496",0,200,0)
 add("Nameless admin REWORKED (@Screatkin)", "Admin commands; More than 280+ admin commands; This is a reworked version no bugs found; Better than old nameless admin and inf yeild.", "https://rawscripts.net/raw/Universal-Script-Nameless-admin-REWORKED-43502",15,15,15)
+add("Nameless admin (@Stars)", "Nameless Admin is a universal admin script, that has over 300+ commands.", "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source",40,40,40)
+add("Orca modded (@moma1133222)", "K to open/close; a script i modded and replaced unworking stuff.", "https://raw.githubusercontent.com/moma1133222/orca-v2/refs/heads/main/orca%20v2%20lol",200,0,200)
